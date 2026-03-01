a3b20c3f4c52e453b9012d8011dc3eb1b4ee2683
TheOddCell <me@oddcell.ca>
2026-03-01 11:57:52 -0500

    ooooops

---
+++ panik.sh
@@ -0,0 +1,25 @@
+if ! command -v insmod >/dev/null 2>&1; then
+    echo "ERROR: insmod not found"
+    exit 1
+fi
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "ERROR: not root"
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    echo "ERROR: no panik message specified"
+    exit 1
+fi
+
+printf "$@" > /tmp/panik
+
+cat <(echo "$PANIK_B64"|base64 -d) > /tmp/panik.ko
+insmod /tmp/panik.ko
+
+sleep 1
+
+echo "ERROR: kernel did not panic"
+dmesg
+exit 1
